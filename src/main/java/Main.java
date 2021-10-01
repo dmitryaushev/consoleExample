d71bda828337c0e13d2f84929d56c2ae4bf7c042
dmitryaushev <dmitryaushev@gmail.com>
2021-10-01 20:09:12 +0300

    Add entry point

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import config.DatabaseConnector;
+import controller.MainController;
+import util.Console;
+import util.View;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        View view = new Console();
+        DatabaseConnector dbConnector = new DatabaseConnector();
+        MainController mainController = new MainController(view, dbConnector.getDataSource());
+        mainController.read();
     }
 }
