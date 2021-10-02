b501c9a9f6d8387460717333d9f4cae253be2e19
dmitryaushev <dmitryaushev@gmail.com>
2021-10-02 10:25:38 +0300

    Update column developer.name

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -3,8 +3,11 @@ create
 
 create table developer
 (
-    id     int auto_increment primary key ,
+    id     int auto_increment primary key,
     name   varchar(255),
     age    int,
     gender enum ('MALE', 'FEMALE')
 );
+
+alter table developer
+    modify name varchar(255) unique;
